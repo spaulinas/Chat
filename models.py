4716ddff366cb467129710065f9a1d9c9a1f38d5
Paulina S <paulinaaszczygiel@gmail.com>
2019-09-27 16:27:39 +0200

    Update models.py

--- models.py
+++ models.py
@@ -12,3 +12,11 @@ class Wiadomosc(models.Model):
     data_pub = models.DateTimeField('data publikacji', auto_now_add=True)
     autor = models.ForeignKey(User)
     
+ class Meta:  # ustawienia dodatkowe
+        verbose_name = u'wiadomość'  # nazwa obiektu w języku polskim
+        verbose_name_plural = u'wiadomości'  # nazwa obiektów w l.m.
+        ordering = ['data_pub']  # domyślne porządkowanie danych
+
+    def __str__(self):
+        return self.tekst  # "autoprezentacja"
+    
